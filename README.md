886e2ec68987a0a0867d318b89dc3228e16093eb
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-11-13 22:42:57 +0200

    fix: update readme

--- README.md
+++ README.md
@@ -16,6 +16,25 @@ Starter for next generation fronted applications
 -   [ ] cypress for testing setup
 -   [ ] ....
 
+#### Getting started
+
+```bash
+pnpm dlx degit veritem/upro project_name
+
+cd project_name
+```
+
+```bash
+pnpm install
+
+
+pnpm dev
+```
+
+- Rename project name in `package.json` 
+
+
+
 #### Contribute
 
 Have an idea in mind about this project consider firing an issue or a PR
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,9 @@
 import Nav from '@/components/Nav'
-import { NextPage } from 'next'
+import {NextPage} from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Fragment } from 'react'
+import {Fragment} from 'react'
+import {GitHub} from 'react-feather'
 
 const About: NextPage = () => {
     return (
@@ -14,12 +15,36 @@ const About: NextPage = () => {
             <section className="font-primary text-center grid place-content-center h-screen w-screen">
                 <Nav />
                 <p className="py-2">
-                    Web fronted starter to quickly build web apps faster
+                    Web fronted starter to swiftly build web apps faster
                 </p>
-                <p>
-                    check it out on{' '}
+
+                <ul className="text-left py-4 list-decimal px-10">
+                    <li>
+                        Next.js
+                    </li>
+                    <li>
+
+                        React query
+                    </li>
+
+                    <li>
+                        TailwindCSS
+                    </li>
+                    <li>
+                        Prettier
+                    </li>
+                    <li>
+                        ESLint
+                    </li>
+                    <li>
+
+                        TypeScript
+                    </li></ul>
+
+
+                <p className="text-center">
                     <Link href="https://github.com/veritem/upro">
-                        <a className="text-blue-500">github</a>
+                        <a className="text-gray-500"><GitHub /></a>
                     </Link>
                 </p>
             </section>
