a6b9bfca31c69c93142b6fe5b5fbae3b83243e07
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-12-10 14:17:50 +0200

    chore:update import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '@/styles/globals.css'
-import {ThemeProvider} from 'next-themes'
-import type {AppProps} from 'next/app'
-import {QueryClient, QueryClientProvider} from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryclient = new QueryClient()
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider attribute="class">
             <QueryClientProvider client={queryclient}>
