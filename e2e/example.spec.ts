ebebc44ac109d917f7d6d4ea048dccfa8cdc81a0
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-09-08 10:22:40 +0200

    chore: migrated to playwright and updated deps

---
+++ e2e/example.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+test('should navigate to the about page', async ({ page }) => {
+    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+    await page.goto('/')
+    // Find an element with the text 'About Page' and click on it
+    await page.click('text=About')
+    // The new url should be "/about" (baseURL is used there)
+    await expect(page).toHaveURL('/about')
+    // The new page should contain an h1 with "About Page"
+})
