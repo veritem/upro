9d46927dcf9cd460ff9c5e3134dc650062985582
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-11-10 15:38:02 +0200

    chore: remove unfinished rules

--- sql-visualizer/src/main.tsx
+++ sql-visualizer/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './style.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
