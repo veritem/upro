361aa0bd1b1994d6eac28d32fb893a1a614ecf2b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-01-08 22:55:18 +0200

    chore: fixed build

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,21 @@
 'use client'
-
 import Nav from '@/components/Nav'
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Fragment } from 'react'
 
-const Home: NextPage = () => {
-    return (
-        <Fragment>
-            <Head>
-                <title>upro</title>
-            </Head>
+export default function Home() {
+	return (
+		<Fragment>
+			<Head>
+				<title>upro</title>
+			</Head>
 
-            <section className="flex justify-center flex-col items-center h-screen w-screen font-primary">
-                <h1 className="font-bold text-5xl py-8">upro</h1>
+			<section className="flex justify-center flex-col items-center h-screen w-screen font-primary">
+				<h1 className="font-bold text-5xl py-8">upro</h1>
 
-                <Nav />
-            </section>
-        </Fragment>
-    )
+				<Nav />
+			</section>
+		</Fragment>
+	)
 }
 
-export default Home
