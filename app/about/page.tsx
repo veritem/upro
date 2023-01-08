0e027417a9cd3f9785178c3fbd8039215da31731
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-01-08 23:11:54 +0200

    chore: fix head component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 import Nav from '@/components/Nav'
 import { NextPage } from 'next'
-import Head from 'next/head'
 import Link from 'next/link'
 import { Fragment } from 'react'
 import { GitHub, Twitter } from 'react-feather'
@@ -8,17 +7,12 @@ import { GitHub, Twitter } from 'react-feather'
 const About: NextPage = () => {
 	return (
 		<Fragment>
-			<Head>
-				<title>about</title>
-			</Head>
-
 			<section className="font-primary text-center grid place-content-center h-screen w-screen">
 				<Nav />
 
 				<ul className="text-left py-4 list-decimal px-10">
 					<li>Next.js</li>
 					<li>React query</li>
-
 					<li>TailwindCSS</li>
 					<li>Prettier</li>
 					<li>ESLint</li>
@@ -28,8 +22,7 @@ const About: NextPage = () => {
 				<div className="flex items-center justify-center gap-4">
 					<Link
 						href="https://github.com/veritem/upro"
-						className="text-gray-500"
-					>
+						className="text-gray-500">
 						<GitHub />
 					</Link>
 
