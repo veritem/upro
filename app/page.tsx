68365d7d6b0be5979cabecae7908aeaa4736e798
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-01-08 22:58:09 +0200

    chore: fixed build

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,17 @@ import Head from 'next/head'
 import { Fragment } from 'react'
 
 export default function Home() {
-	return (
-		<Fragment>
-			<Head>
-				<title>upro</title>
-			</Head>
+    return (
+        <Fragment>
+            <Head>
+                <title>upro</title>
+            </Head>
 
-			<section className="flex justify-center flex-col items-center h-screen w-screen font-primary">
-				<h1 className="font-bold text-5xl py-8">upro</h1>
+            <section className="flex justify-center flex-col items-center h-screen w-screen font-primary">
+                <h1 className="font-bold text-5xl py-8">upro</h1>
 
-				<Nav />
-			</section>
-		</Fragment>
-	)
+                <Nav />
+            </section>
+        </Fragment>
+    )
 }
-
